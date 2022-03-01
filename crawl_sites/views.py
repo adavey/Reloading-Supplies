4024514fc1e6d59f89b0edccfd7f7cb8fd3d6736
Alan Davey <alan.davey@numerator.com>
2022-02-28 23:16:36 -0500

    created index page w/ simple stats

--- crawl_sites/views.py
+++ crawl_sites/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Manufacturer, Product, Website, Website_Product
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_manufactures = Manufacturer.objects.all().count()
+    num_products = Product.objects.all().count()
+    num_websites = Website.objects.all().count()
+    num_webProducts = Website_Product.objects.all().count()
+
+    context = {
+        'num_manufactures': num_manufactures,
+        'num_products': num_products,
+        'num_websites': num_websites,
+        'num_webProducts': num_webProducts,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
